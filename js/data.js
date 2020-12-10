6b2d39107c9a343e00ba080841b35140890684e1
Alessandro <alessandro.mancinelli89@gmail.com>
2020-12-10 12:48:42 +0100

    Footer done

--- js/data.js
+++ js/data.js
@@ -1,25 +1,83 @@
-const navbar = ["HOME", "PAGES", "PROGRAM", "TICKETS", "SPEAKERS", "PAPERS", "BLOG", "SHORTCODES"];
+const navbar = ["Home", "Pages", "Program", "Tickets", "Speakers", "Papers", "Blog", "Shortcodes"];
 
 const speakers = [
     {
-        img: 'img/speaker-6.jpg',
-        name: 'PATRICK SPENCER',
-        role: 'S&P Analyzer'
+        img: 'img/speaker-1.jpg',
+        name: 'Jonathan Elves',
+        role: "Twitter's Co-Founder"
     },
     {
-        img: 'img/speaker-5.jpg',
-        name: 'JANET JONES',
-        role: "Newyork Post's GM"
-    },
-    {
-        img: 'img/speaker-4.jpg',
-        name: 'MICHAEL DOVER',
-        role: "Starbuck's CEO"
+        img: 'img/speaker-2.jpg',
+        name: 'Matthew White',
+        role: "Facebook's Co-Founder"
     },
     {
         img: 'img/speaker-3.jpg',
-        name: 'ANGELINA HOLY',
+        name: 'Angelina Holy',
         role: "Maxii's Manager"
     },
+    {
+        img: 'img/speaker-4.jpg',
+        name: 'Michael Dover',
+        role: "Starbuck's CEO"
+    },
+    {
+        img: 'img/speaker-5.jpg',
+        name: 'Janet Jones',
+        role: "Newyork Post's GM"
+    },
+    {
+        img: 'img/speaker-6.jpg',
+        name: 'Patrick Spencer',
+        role: 'S&P Analyzer'
+    },
+    {
+        img: 'img/speaker-7.jpg',
+        name: 'John Super Doe',
+        role: "Yahoo's HR"
+    },
+    {
+        img: 'img/speaker-8.jpg',
+        name: 'Laurence Francis',
+        role: "Google's UI Designer"
+    },
+];
 
-]
+const socials = [
+    "fab fa-facebook-square",
+    "fab fa-twitter-square",
+    "fab fa-linkedin",
+    "fab fa-google-plus-square",
+    "fab fab fa-pinterest-square",
+    "fab fab fa-flickr",
+];
+
+const footerLinks = [
+    "SEM PORTA MOLLIS PARTURIENT",
+    "NULLAM LOREM MATTIS PURUS",
+    "NIBH SEM SIT ULLAMCORPER",
+    "DONEC LUCTUS IMPERDIET",
+    "MAGNA PARS STUDIORUM",
+];
+
+const photoGrid = [
+    "img/photogrid-1.jpg",
+    "img/photogrid-2.jpg",
+    "img/photogrid-3.jpg",
+    "img/photogrid-4.jpg",
+    "img/photogrid-5.jpg",
+    "img/photogrid-6.jpg",
+    "img/photogrid-7.jpg",
+    "img/photogrid-8.jpg",
+    "img/photogrid-9.jpg",
+    "img/photogrid-10.jpg",
+    "img/photogrid-11.jpg",
+    "img/photogrid-12.jpg",
+];
+
+const copyrightLinks = [
+    "About Us",
+    "Accomodation",
+    "Call For Paper",
+    "Contact Us"
+];
--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
+/* 
+Riprodurre il layout desktop del tema wordpress "The keynote"
+*/
+
 const app = new Vue({
     el: '#root',
     data: {
         header: [...navbar],
         selectedMenu: '',
         speakers: [...speakers],
+        socials: [...socials],
+        footerLinks: [...footerLinks],
+        photos: [...photoGrid],
+        copyrightLinks: [...copyrightLinks]
     },
     methods: {
         selectMenu(link) {
